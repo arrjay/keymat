9b2d8b394b26e875c8ebd402394cf7ddcff41b53
RJ Bergeron <rbergero@gmail.com>
2018-03-12 00:21:36 -0700

    write down the restore hack

---
+++ restore-private-key.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+case "$(uname -o 2>/dev/null)" in
+  Android)
+cat << _EOF_
+This script will read the android clipboard via termux-api and _should_
+import a gpg public key for your keyring.
+_EOF_
+
+read -n 1 -s -r -p "Press any key to start reading clipboard"
+./termux-read-data.sh | base64 -d > paperkey.secret
+  ;;
+  *)
+./zbarimg-read-data.sh private-ss.pdf | base64 -D > paperkey.secret
+  ;;
+esac
+
+paperkey --pubring pubkey.gpg --secrets paperkey.secret --output seckey.gpg
